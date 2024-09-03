16cfc399b6c3b49825e0eb6473de057fb06193d8
srishti soni <srishtisoni7541@gmail.com>
2024-09-03 09:49:47 +0530

    some change

--- script.js
+++ script.js
@@ -25,31 +25,15 @@ tl.to('.h1text h1',{
 })
 
 
-
-var services=document.querySelector('.navright .services');
-services.addEventListener('mouseover',function(){
-    gsap.to('.servicemenu',{
-        opacity:1,
-        // duration:.5,
-    })
-})
-    services.addEventListener('mouseleave',function(){
-        gsap.to('.servicemenu',{
-            opacity:0,
-            duration:.5,
-        })
-    })
-
-
 // Select the elements
-var menubar = document.querySelector(' .page1 .navbtm #icon');
+var menubar = document.querySelector('.page1 nav i');
 menubar.addEventListener('click',function(){
     gsap.to('.page1 .menucontainer',{
         left:0,
         duration:.5,
     })
 })
-var closebtn =document.querySelector('.menucontainer i');
+var closebtn = document.querySelector('.menucontainer i');
 closebtn.addEventListener('click',function(){
     gsap.to('.menucontainer',{
         left:'-100%',
@@ -57,55 +41,6 @@ closebtn.addEventListener('click',function(){
     })
 })
 
-
-
-
-
-function page2Animation(){
-    gsap.to('.h2textcontainer h2',{
-        y:-70,
-        duration:.3,
-        ease:'expo.Inout',
-        scrollTrigger:{
-            trigger:'.page2',
-            // scrub:true
-        }
-    })
-}
- page2Animation();
-
-
- function page3Animation(){
-    gsap.to('.page3righttop h3',{
-        x:-450,
-        duration:.8,
-        opacity:1,
-        ease:'expo.Inout',
-        scrollTrigger:{
-            trigger:'.page3',
-            scroller:'body',
-        }
-    })
- }
-
- page3Animation();
-
-
- function page4Animation(){
-    gsap.to('.h5text h5',{
-        x:650,
-        duration:.8,
-        // opacity:1,
-        ease:'expo.Inout',
-        scrollTrigger:{
-            trigger:'.page4',
-            scroller:'body',
-            start:'top 20%'
-        }
-    })
- }
-
- page4Animation();
 
 
 
